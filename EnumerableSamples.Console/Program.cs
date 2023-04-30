f47fe530fb8acd0e380e9c513cc5b759f1de4a59
LuisPulenta <luisalbertonu@gmail.com>
2023-04-30 14:49:01 -0300

    Linked Lists

--- EnumerableSamples.Console/Program.cs
+++ EnumerableSamples.Console/Program.cs
@@ -77,38 +77,82 @@ Console.WriteLine("-----------");
 #endregion
 
 #region Dictionary2
-Console.WriteLine("-------------------------------");
-Console.WriteLine("  Dictionary<TKey, TValue>  ");
-Console.WriteLine("-------------------------------");
+//Console.WriteLine("-------------------------------");
+//Console.WriteLine("  Dictionary<TKey, TValue>  ");
+//Console.WriteLine("-------------------------------");
 
-var products = new Dictionary<int, Product>
+//var products = new Dictionary<int, Product>
+//{
+//    { 1, new Product{Id=1,Name="Coca Cola",Price=2.15M} },
+//    { 2, new Product{Id=2,Name="Leche",Price=5.58M} },
+//    { 3, new Product{Id=3,Name="IPhone",Price=354.99M} },
+//    { 4, new Product{Id=4,Name="IWatch",Price=121.99M} },
+
+//};
+
+
+
+//foreach (var product in products)
+//{
+//    Console.WriteLine($"{product.Key} => {product.Value}");
+//}
+
+//if (products.ContainsKey(5))
+//{
+//    products[5].Price *= 1.10M;
+//}
+//else
+//{
+//    products.Add(5, new Product { Id = 5, Name = "Camisa", Price = 33.33M });
+//}
+//Console.WriteLine("-------------------------------");
+
+//foreach (var product in products)
+//{
+//    Console.WriteLine($"{product.Key} => {product}");
+//}
+#endregion
+
+#region LinkedList
+Console.WriteLine("------------");
+Console.WriteLine("Linked Lists");
+Console.WriteLine("------------");
+
+var numbers = new LinkedList<int>();
+
+numbers.AddFirst(5);
+numbers.AddLast(10);
+numbers.AddLast(20);
+numbers.AddLast(30);
+numbers.AddLast(40);
+numbers.AddLast(50);
+numbers.AddLast(60);
+numbers.AddLast(65);
+
+foreach (var number in numbers)
 {
-    { 1, new Product{Id=1,Name="Coca Cola",Price=2.15M} },
-    { 2, new Product{Id=2,Name="Leche",Price=5.58M} },
-    { 3, new Product{Id=3,Name="IPhone",Price=354.99M} },
-    { 4, new Product{Id=4,Name="IWatch",Price=121.99M} },
+    Console.WriteLine(number);
+}
+Console.WriteLine("------------");
 
-};
+var node30 = numbers.Find(30);
+numbers.AddAfter(node30!, 35);
 
+var node50 = numbers.Find(50);
+numbers.Remove(node50!);
 
-
-foreach (var product in products)
-{
-    Console.WriteLine($"{product.Key} => {product.Value}");
+var pointer = numbers.Last;
+while (pointer != null){
+    Console.WriteLine(pointer.Value);
+    pointer = pointer.Previous;
 }
 
-if (products.ContainsKey(5))
+Console.WriteLine("------------");
+var pointer2 = numbers.First;
+while (pointer2 != null)
 {
-    products[5].Price *= 1.10M;
+    Console.WriteLine(pointer2.Value);
+    pointer2 = pointer2.Next;
 }
-else
-{
-    products.Add(5, new Product { Id = 5, Name = "Camisa", Price = 33.33M });
-}
-Console.WriteLine("-------------------------------");
 
-foreach (var product in products)
-{
-    Console.WriteLine($"{product.Key} => {product}");
-}
 #endregion
